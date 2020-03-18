697a63e5c0f26c6060f541204ea573623bc64b16
Serkan Kaan Bahsi <serkankaanbahsi@gmail.com>
2020-03-18 23:32:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# C-sharp
+***
+# Table of Contents
+* [Description](#Description)
+* [Contributing](#Contributing)
+* [Authors](#Authors)
+* [Contact](#Contact)
+***
+# Description
+* This repository contains my C-sharp examples.
+***
+# Contributing
+* All ideas and helps are welcome. Please contact me.
+# Authors
+* Serkan Kaan Bahşi
+# Contact
+* [@SerkanKaanBahsi](https://github.com/SerkanKaanBahsi/) on **GitHub**
+* [@serkan-kaan-bahsi](https://www.linkedin.com/in/serkan-kaan-bahsi/) on **LinkedIn**
---
+++ csharp-examples/E001_Control_Statements/Control_Statements/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Basic control statements in c-sharp
+
+namespace Control_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int var = Convert.ToInt32(Console.ReadLine());
+
+            if (var == 10)
+            {
+                Console.WriteLine("Your Number is 10");
+            }
+            else if (var == 5)
+            {
+                Console.WriteLine("Your Number is 5");
+            }
+            else
+            {
+                Console.WriteLine("I dont know");
+            }
+
+            switch (var)
+            {
+                case 5: Console.WriteLine("It is 5"); break;
+                case 10: Console.WriteLine("It is 10"); break;
+                default: Console.WriteLine("Not 5 or 10"); break;
+            }
+
+            for (int j = 1; j <= 10; j++)
+            {
+                if (j == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine(j);
+            }
+
+            while (var >= 0)
+            {
+                Console.WriteLine("Not yet");
+                var--;
+            }
+            Console.WriteLine("Press Any Key to Exit");
+            Console.ReadKey();
+        }
+    }
+}
